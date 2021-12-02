fb68e8e35f68570d483279ee044bdc4a9713a529
thao.nguyen15 <thao.nguyen15@onemount.com>
2021-12-02 23:02:52 +0700

    first commit

---
+++ src/main/java/vn/onehs/property/OnehsPropertyApplication.java
@@ -0,0 +1,13 @@
+package vn.onehs.property;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnehsPropertyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnehsPropertyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/onehs/property/OnehsPropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.onehs.property;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnehsPropertyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
