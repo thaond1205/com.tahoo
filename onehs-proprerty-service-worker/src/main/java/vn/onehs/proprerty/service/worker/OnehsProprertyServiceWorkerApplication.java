37d9eb82142ec0282f8d60d496f3d7c38ed6845b
thao.nguyen15 <thao.nguyen15@onemount.com>
2022-01-12 16:23:16 +0700

    add config worker

---
+++ onehs-proprerty-service-worker/src/main/java/vn/onehs/proprerty/service/worker/OnehsProprertyServiceWorkerApplication.java
@@ -0,0 +1,13 @@
+package vn.onehs.proprerty.service.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnehsProprertyServiceWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnehsProprertyServiceWorkerApplication.class, args);
+    }
+
+}
---
+++ onehs-proprerty-service-worker/src/test/java/vn/onehs/proprerty/service/worker/OnehsProprertyServiceWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.onehs.proprerty.service.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnehsProprertyServiceWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
