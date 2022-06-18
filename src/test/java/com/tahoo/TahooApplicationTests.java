c0edb65ac9a9d56eb417a849bff8fd5a36c34706
thaond12052001 <thaond1205@gmail.com>
2022-06-18 10:03:30 +0700

    First commit, create package

---
+++ src/test/java/com/tahoo/TahooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tahoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TahooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
